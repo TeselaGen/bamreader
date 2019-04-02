382b31fd24744131a3e66685b2eb5367f5ab9480
tiffanydai <tiffany.dai@teselagen.com>
2019-04-02 11:11:36 -0700

    adding reversed property to seq reads

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 const BAMLineReader = require("./bamreader");
 
 function readBAM(providedPath) {
-    return new Promise((resolve, reject) => {
-        let seqReads = [];
-        BAMLineReader.create(providedPath).on("bam", function(bam, dOffset, iOffset){
-            seqReads.push({
-                name: bam.qname,
-                seq: bam.seq,
-                pos: bam.pos,
-                cigar: bam.cigar
-            })
-            // seqReads should be an array of objects [{name, seq, pos, cigar}, {name, seq, pos, cigar}, ...]
-            return seqReads;
-        }).on("end", function() {
-            resolve(seqReads)
+  return new Promise((resolve, reject) => {
+    let seqReads = [];
+    BAMLineReader.create(providedPath)
+      .on("bam", function(bam, dOffset, iOffset) {
+        seqReads.push({
+          name: bam.qname,
+          seq: bam.seq,
+          pos: bam.pos,
+          cigar: bam.cigar,
+          reversed: bam.reversed
         });
-    })
+        // seqReads should be an array of objects [{name, seq, pos, cigar, reversed}, {name, seq, pos, cigar}, ...]
+        return seqReads;
+      })
+      .on("end", function() {
+        resolve(seqReads);
+      });
+  });
 }
 
 module.exports = readBAM;
